39b5dfcbb0d21a1d55292e441c49c4e0bddc8f51
kinstaky <kinstaky@163.com>
2026-01-16 16:35:40 +0800

    update namespace

--- extensions/attpc_flow_extension.cpp
+++ extensions/attpc_flow_extension.cpp
@@ -5,17 +5,17 @@
 namespace nb = nanobind;
 using namespace nb::literals;
 
-bool check_graw(std::string graw_dir, std::string workspace_dir, int run) {
-	flow::GrawChecker checker(graw_dir, workspace_dir, run);
-	flow::CheckGrawResult result = checker.Check();
+bool check_graw_event_id(std::string graw_dir, std::string workspace_dir, int run) {
+	atflow::GrawChecker checker(graw_dir, workspace_dir, run);
+	atflow::CheckGrawResult result = checker.Check();
 	return result.pass;
 }
 
 NB_MODULE(attpc_flow_ext, m) {
 	m.doc() = "Python extensions written in C++ for attpc flow.";
 	m.def(
-		"check_graw",
-		&check_graw,
+		"check_graw_event_id",
+		&check_graw_event_id,
 		"graw_dir"_a, "workspace_dir"_a, "run"_a,
 		"This function check single run graw files."
 	);
